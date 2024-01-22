e97dd52e093af254cd0aa6ba990a01aa57f18457
Jack Maders <jackwmaders@gmail.com>
2024-01-22 22:18:42 +0000

    add eslint & prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/test-utils/module"],
+  modules: ["@nuxt/test-utils/module", "@nuxtjs/eslint-module"],
   experimental: {
     // prevent error in unit test github action
     appManifest: false,
+  },
+  eslint: {
+    // prevent warning on startup
+    lintOnStart: false,
   },
 });
