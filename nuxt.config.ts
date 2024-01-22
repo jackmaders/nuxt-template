2db7dacdd02b85ebfef50a7d85a1f3aff56c596c
Jack Maders <jackwmaders@gmail.com>
2024-01-22 22:20:20 +0000

    add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/test-utils/module", "@nuxtjs/eslint-module"],
+  modules: [
+    "@nuxt/test-utils/module",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/eslint-module",
+  ],
   experimental: {
     // prevent error in unit test github action
     appManifest: false,
+  },
+  tailwindcss: {
+    // prevent message on startup
+    viewer: false,
   },
   eslint: {
     // prevent warning on startup
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+};
