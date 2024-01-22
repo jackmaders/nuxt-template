82643a8f7ccc291981921acb9896ca7c40572aca
Jack Maders <jackwmaders@gmail.com>
2024-01-22 22:17:17 +0000

    add vitest & @nuxt/test-utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxt/test-utils/module"],
+  experimental: {
+    // prevent error in unit test github action
+    appManifest: false,
+  },
+});
